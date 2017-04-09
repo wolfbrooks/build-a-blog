4aad096038a28fe2de9dc1bf78b84a9eff7a7f3b
Brianna Greene <briannagreene7@gmail.com>
2017-04-09 12:32:47 -0500

    added templates and the BlogPage handler

--- main.py
+++ main.py
@@ -15,11 +15,71 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+from google.appengine.ext import db
 
-class MainHandler(webapp2.RequestHandler):
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class Entries(db.Model):
+    title = db.StringProperty(required = True)
+    entry = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class MainPage(Handler):
+    def render_front(self, title="", entry="", error=""):
+        entries = db.GqlQuery("SELECT * FROM Entries " "ORDER BY created DESC LIMIT 5 ")
+        self.render("front.html", title=title, entry=entry, error=error, entries=entries)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_front()
+
+    def post(self):
+        title = self.request.get('title')
+        entry = self.request.get('entry')
+
+        if title and entry:
+            a = Entries(title = title, entry = entry)
+            a.put()
+
+            self.redirect("/blog")
+
+        else:
+            error = "We need both a title and some content!"
+            self.render_front(title, entry, error)
+
+class BlogPage(Handler):
+    def render_front(self, title="", entry="", error=""):
+        entries = db.GqlQuery("SELECT * FROM Entries " "ORDER BY created DESC LIMIT 5 ")
+        self.render("blog-submissions.html", title=title, entry=entry, error=error, entries=entries)
+
+    def get(self):
+        title = self.request.get('title')
+        entry = self.request.get('entry')
+
+        if title and entry:
+            a = Entries(title = title, entry = entry)
+            a.put()
+
+            self.redirect("/blog")
+
+        else:
+            error = "We need both a title and some content!"
+            self.render_front(title, entry, error)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage),
+    ('/blog', BlogPage)
 ], debug=True)
